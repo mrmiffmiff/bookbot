2b758ccc99fb911c0922655aec6859f728555491
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-09-12 12:40:33 -0700

    Move word count function to own file

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import get_book_word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-
-def get_book_word_count(book_text):
-    text_list = book_text.split()
-    return len(text_list)
 
 def main():
     contents = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_book_word_count(book_text):
+    text_list = book_text.split()
+    return len(text_list)
