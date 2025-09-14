9a28fc4b33e04ed5ce6aefbb0b1c292e7ac04285
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-09-14 14:34:32 -0700

    implement second sysarg as filepath

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_book_word_count, get_book_char_counts, sort_chars
 
 def get_book_text(filepath):
@@ -6,9 +7,13 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-    contents = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_file = sys.argv[1]
+    contents = get_book_text(path_to_file)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}...")
     word_count = get_book_word_count(contents)
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
